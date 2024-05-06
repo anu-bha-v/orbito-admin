ec1c34a686d5088196c76e19070653dc919ec55e
anubhaverma13 <anubhavverma9919@gmail.com>
2024-05-06 19:57:06 +0530

    first

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
-const port = process.env.PORT || 10000 ;
+const port = process.env.PORT || 4000 ;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
